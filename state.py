b60681ca98c0b71d9260ddcadd9f0b9a37401b30
uczen <tobimalyszek@gmail.com>
2025-11-14 20:29:09 +0100

    state

--- state.py
+++ state.py
@@ -11,6 +11,9 @@ class State:
         bar_width = int(250 * self.reactor_sanity / self.max_reactor_sanity)
         pygame.draw.rect(screen, (255, 0, 0), (50, 500, 250, 20))
         pygame.draw.rect(screen, (0, 255, 0), (50, 500, bar_width, 20))
+        font = pygame.font.SysFont("Calibri", 24)
+        text = font.render(f"Reactor Sanity: {self.reactor_sanity} / {self.max_reactor_sanity}", True, (0, 0, 0))
+        screen.blit(text, (55, 500))
 
 state = State()
 
