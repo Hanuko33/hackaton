86fc6719ac705f1a0f62f6fd4e5c7bc658b3ad85
Piotr Danecki <i3riced@mailfence.com>
2025-11-15 07:43:41 +0100

    add polish version for intro
    Signed-off-by: Piotr Danecki <i3riced@mailfence.com>

--- intro.py
+++ intro.py
@@ -21,7 +21,7 @@ times_text = {
     1900: 'This brave soul was known as "Uranek," the beacon of defiance...',
     2100: "Though he stood alone, he carried the weight of a world's fate...",
     2300: "Now, the battle for survival rages on.",
-    2500: "Every day, Uranek confronts the chaos within, striving to reclaim what was lost...",
+    2500: "Every day, Uranek confronts the chaos, striving to reclaim what was lost...",
     2700: "He tirelessly works to stabilize the reactor, seeking to restore balance...",
     2800: "...",
     2900: "...",
@@ -29,13 +29,39 @@ times_text = {
     3100: "Will you join him in this fight for redemption?"
 }
 
+times_text_pl = {
+    100: "W łagodnej krainie Uranex, harmonia wspaniale kwitła wśród jej mieszkańców.",
+    300: "Żyli w błogiej nieświadomości nadciągających cieni, które zagrażały ich egzystencji.",
+    600: "Lecz nagle, bez ostrzeżenia, katastrofa uderzyła w samą istotę ich świata.",
+    800: "Kryzys uwolnił chaos, rozdzierając ich spokój na strzępy.",
+    1000: "...",
+    1200: "Niegdyś tętniące życiem istoty zostały wrzucone do reaktora",
+    1300: "zatrutego ciemną energią, która zniekształcała ich esencję.",
+    1600: "Jednak w obliczu zepsucia, iskra nadziei zapłonęła...",
+    1700: "Jeden z nich odważył się stanąć przeciw narastającej ciemności.",
+    1800: "...",
+    1900: 'Ten odważny duch był znany jako "Uranek", latarnia oporu...',
+    2100: "Choć stał sam, dźwigał ciężar losu całego świata...",
+    2300: "A teraz walka o przetrwanie trwa w nieustannym zrywie.",
+    2500: "Każdego dnia Uranek stawia czoła chaosowi, usiłując odzyskać to, co utracone...",
+    2700: "Niezłomnie pracuje nad stabilizacją reaktora, pragnąc przywrócić równowagę...",
+    2800: "...",
+    2900: "...",
+    3000: "...",
+    3100: "Czy dołączysz do niego w tej bohaterskiej walce o odkupienie?"
+}
+
+
 clock = pygame.time.Clock()
+print(times_text)
+print(times_text_pl)
 
 
 def intro(screen, font):
     music.play_intro()
     global running
     global t
+    polish = False
     while running:
         (SCREEN_WIDTH, SCREEN_HEIGHT) = screen.get_size()
         background_ = pygame.transform.scale(
@@ -48,10 +74,15 @@ def intro(screen, font):
                 "press space or any mouse button to skip", True, (50, 50, 50))
             screen.blit(txt, (SCREEN_WIDTH - txt.get_width(),
                         SCREEN_HEIGHT - txt.get_height()))
+            txt = font.render(
+                "press p for polish version", True, (50, 50, 50))
+            screen.blit(txt, (SCREEN_WIDTH - txt.get_width(),
+                        SCREEN_HEIGHT - txt.get_height() * 2))
         i = 0
-        for tt in times_text:
+        operating = times_text_pl if polish else times_text
+        for tt in operating:
             if t > tt:
-                txt = font.render(times_text[tt], True, (255, 255, 255))
+                txt = font.render(operating[tt], True, (255, 255, 255))
                 screen.blit(txt, (5, txt.get_height() * i + 5))
             i += 1
         if t > 4000:
@@ -62,6 +93,9 @@ def intro(screen, font):
             if event.type == KEYDOWN:
                 if event.key == pygame.K_SPACE:
                     running = False
+                if event.key == pygame.K_p:
+                    polish ^= 1
+                    polish = bool(polish)
             if event.type == MOUSEBUTTONDOWN:
                 running = False
         pygame.display.update()
